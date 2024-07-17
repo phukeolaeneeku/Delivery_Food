412c79f9f30a813111283267b950511cdebf68ee
phukeolaeneeku <144010043+phukeolaeneeku@users.noreply.github.com>
2024-07-17 15:50:39 +0700

    Update in management order

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 80, // you can replace this port with any port
+    port: 3000, // you can replace this port with any port
   }
 
 });
